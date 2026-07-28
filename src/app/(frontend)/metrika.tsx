185763c6096f8d51a7d66a363fb99ff39103904d
Alexander Demyankov <alexander.demyankov@b8.dev>
2026-07-28 10:02:42 +0300

    perf: load Yandex Metrika on idle and preconnect to its origin
    Webvisor session recording attaches MutationObservers as soon as the script
    runs, and afterInteractive fires right at hydration, competing with the
    app's own hydration for main-thread time. lazyOnload defers it to idle.

--- src/app/(frontend)/metrika.tsx
+++ src/app/(frontend)/metrika.tsx
@@ -7,7 +7,8 @@ export function Metrika() {
 
   return (
     <>
-      <Script id="yandex-metrika" strategy="afterInteractive">
+      <link rel="preconnect" href="https://mc.yandex.ru" crossOrigin="" />
+      <Script id="yandex-metrika" strategy="lazyOnload">
         {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],
         k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
